14f6f4f677b1082c53621afb5642204638a0269e
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-02-09 13:26:20 +0000

    Base implementation of the word filter service
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/wordlists/trunk@29851 735d13b6-9817-0410-8766-e36946ffe9aa

--- sv1/class.tx_wordlists_sv1.php
+++ sv1/class.tx_wordlists_sv1.php
@@ -21,11 +21,6 @@
 *
 *  This copyright notice MUST APPEAR in all copies of the script!
 ***************************************************************/
-/**
- * [CLASS/FUNCTION INDEX of SCRIPT]
- *
- * Hint: use extdeveval to insert/update function index above.
- */
 
 require_once(PATH_t3lib.'class.t3lib_svbase.php');
 
@@ -33,49 +28,116 @@ require_once(PATH_t3lib.'class.t3lib_svbase.php');
 /**
  * Service "White lists/Black lists" for the "wordlists" extension.
  *
- * @author	Francois Suter (Cobweb) <typo3@cobweb.ch>
- * @package	TYPO3
+ * @author		Francois Suter (Cobweb) <typo3@cobweb.ch>
+ * @package		TYPO3
  * @subpackage	tx_wordlists
+ *
+ * $Id$
  */
 class tx_wordlists_sv1 extends t3lib_svbase {
-				var $prefixId = 'tx_wordlists_sv1';		// Same as class name
-				var $scriptRelPath = 'sv1/class.tx_wordlists_sv1.php';	// Path to this script relative to the extension dir.
-				var $extKey = 'wordlists';	// The extension key.
+	public $prefixId = 'tx_wordlists_sv1';		// Same as class name
+	public $scriptRelPath = 'sv1/class.tx_wordlists_sv1.php';	// Path to this script relative to the extension dir.
+	public $extKey = 'wordlists';	// The extension key.
+	protected $configuration;
+	protected $listType = 'black';
+	protected $words = array();
 	
-				/**
-	 * [Put your description here]
+	/**
+	 * This method checks whether the service is able to perform its duty or not
 	 *
-	 * @return	[type]		...
+	 * @return	boolean		TRUE or FALSE depending on availability of service
 	 */
-				function init()	{
-					$available = parent::init();
-	
-					// Here you can initialize your class.
-	
-					// The class have to do a strict check if the service is available.
-					// The needed external programs are already checked in the parent class.
-	
-					// If there's no reason for initialization you can remove this function.
-	
-					return $available;
-				}
-	
-				/**
-	 * [Put your description here]
-	 * performs the service processing
+	public function init()	{
+			// Read the extension's configuration
+		$this->configuration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$this->extKey]);
+			// Nothing can make this service unavailable
+		return TRUE;
+	}
+
+	/**
+	 * This method loads a given wordlists record for checking
+	 * The $parameters array is expected to contain a "uid" index which
+	 * points to the uid of a wordlists record
 	 *
-	 * @param	string		Content which should be processed.
-	 * @param	string		Content type
-	 * @param	array		Configuration array
-	 * @return	boolean
+	 * @param	array	$parameters: list of parameters, as needed by the service
+	 * @return	void
 	 */
-				function process($content='', $type='', $conf=array())	{
-	
-					// Depending on the service type there's not a process() function.
-					// You have to implement the API of that service type.
-	
-					return FALSE;
+	public function load($parameters) {
+			// Reset the type of list and the list of words
+			// Note that these default values make it so that every word is valid
+			// (an empty black list means that nothing is excluded)
+		$this->listType = 'black';
+		$this->words = array();
+			// Load the new list given its uid
+		$uid = intval($parameters['uid']);
+			// The give id is empty, issue warning
+		if (empty($uid)) {
+			if (!empty($this->configuration['debug'])) {
+				t3lib_div::devLog('No uid given. All words will be allowed.', $this->extKey, 2);
+			}
+		} else {
+			$record = array();
+			$fields = '*';
+			$table = 'tx_wordlists_lists';
+			$where = "uid = '" . $uid . "'";
+				// If in the FE context, use overlays API to retrieve record
+				// so that we get it automatically overlayed with current language
+			if ($GLOBALS['TYPO3_MODE'] == 'FE') {
+				$records = tx_overlays::getAllRecordsForTable($fields, $table, $where);
+				if (count($records) > 0) {
+					$record = $records[0];
 				}
+
+				// If operating in another context, get record as is
+			} else {
+				$res = $GLOBALS['TYPO3_DB']->exec_SELECTquery($fields, $table, $where);
+				if ($res && $GLOBALS['TYPO3_DB']->sql_num_rows($res) > 0) {
+					$record = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res);
+				}
+			}
+				// If a record was retrieved, load its content into the internal words list
+			if (count($record) > 0) {
+				$this->listType = $record['type'];
+				$lines = t3lib_div::trimExplode("\n", $record['words'], TRUE);
+				foreach ($lines as $aLine) {
+					$words = t3lib_div::trimExplode(',', $aLine, TRUE);
+					foreach ($words as $aWord) {
+						$this->words[] = $aWord;
+					}
+				}
+				$this->words = array_unique($this->words);
+
+				// No record was found, issue a warning
+			} else {
+				if (!empty($this->configuration['debug'])) {
+					t3lib_div::devLog('No word list found for uid ' . $uid . '. All words will be allowed.', $this->extKey, 2);
+				}
+			}
+		}
+	}
+
+	/**
+	 * This method checks whether a given word is valid or not
+	 * according to the loaded white list of black list
+	 *
+	 * @param	string		$word: the word to check
+	 * @return	boolean		TRUE or FALSE depending on the assessed validity of the word
+	 */
+	public function isValidWord($word) {
+		$isValid = FALSE;
+		switch ($this->listType) {
+				// White list: the word is valid if it is in the list of words
+			case 'white':
+				$isValid = in_array($word, $this->words);
+				break;
+
+				// Black list: the word is valid if it is *NOT* in the list of words
+			default:
+				$isValid = !in_array($word, $this->words);
+				break;
+		}
+		return $isValid;
+	}
 }
 
 
